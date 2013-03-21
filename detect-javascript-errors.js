9e202e164e9487fa4eb8f118a50c1bf188dcb6ed
James Young <james.young@turner.com>
2013-03-21 18:12:47 -0400

    changes

--- detect-javascript-errors.js
+++ detect-javascript-errors.js
@@ -1,13 +1,22 @@
-casper.log('Starting javascript-errors.js', 'info');
+/*globals require */
+
+/* usage: casperjs test --direct --log-level=info --disk-cache=no detect-javascript-errors.js */
+
+var casper = require('casper').create({
+    verbose: true,
+    logLevel: 'info'
+});
 
 casper.start('http://www.cnn.com/');
 
-casper.on("page.error", function(msg, trace) {
+casper.on("page.error", function (msg, trace) {
+    'use strict';
+    this.log('found page.error');
     this.test.fail(msg);
 });
 
-casper.run(function() {
+casper.run(function () {
+    'use strict';
     this.log('Starting casper.run()', 'info');
-    this.test.renderResults(true, 0, 'casper-results.xml');
     this.test.done();
 });
